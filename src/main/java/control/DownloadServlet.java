1f9535216b944c146f0682e628999aaf862a50c5
Alex <Alex@192.168.51.2>
2017-07-19 20:30:24 +0700

    up

---
+++ src/main/java/control/DownloadServlet.java
@@ -0,0 +1,34 @@
+package control;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DownloadServlet")
+public class DownloadServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/zip");
+        //response.setContentType("image/jpg");
+        PrintWriter out = response.getWriter();
+        String filename = "DienDanBacLieu.Net_Recuva 1.47.zip";
+        String filepath = "e:\\";
+        response.setContentType("APPLICATION/OCTET-STREAM");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+
+        java.io.FileInputStream fileInputStream = new java.io.FileInputStream(filepath + filename);
+
+        int i;
+        while ((i = fileInputStream.read()) != -1) {
+            out.write(i);
+        }
+        fileInputStream.close();
+        out.close();
+    }
+
+}
---
+++ src/main/webapp/dowload.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : dowload
+    Created on : Nov 16, 2015, 2:24:29 PM
+    Author     : Asus
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+       <a href="DownloadServlet">download the jsp file</a>
+    </body>
+</html>
